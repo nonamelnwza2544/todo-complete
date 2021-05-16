1c0b007368c463de1069f85eb30631a35878dba7
nonamelnwza2544 <pueblo555@hotmail.com>
2021-05-16 14:15:27 +0700

    เพิ่ม กรอ นะ

--- todo-complete/src/App.js
+++ todo-complete/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import CardTodo from "./components/CardTodo";
 
 function App() {
-  return (
-    <div className="App p-grid p-align-start vertical-container">
-      <div className="p-col">
-        <CardTodo />
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "App p-grid p-align-start vertical-container" >
+        <
+        div className = "p-col" >
+        <
+        CardTodo / >
+        <
+        /div> <
+        /div>
+
+    );
 }
 
 export default App;
